7f22e4a1bcd55f9506f89b4755330e8c78da1bbc
binarylinuxx <aar58384@gmail.com>
2026-01-17 18:05:22 +0200

    Fix site URL for GitHub Pages default domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zap-zig.github.io/zap',
+	site: 'https://zap-zig.github.io/zap-docs',
 	base: '/zap-docs',
 });
