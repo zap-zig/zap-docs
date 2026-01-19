213a08af1ed597a93fcbb13bd261b96a996f3a6f
binarylinuxx <aar58384@gmail.com>
2026-01-19 17:30:00 +0200

    fresh root build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'http://zapzig.org',           // ← changed to http for now
-  base: '/zap-docs/',                  // ← Changed to match your repository name
+  base: '/',                  // ← Changed to match your repository name
   trailingSlash: 'never',              
 });
